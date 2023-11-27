86bcf115d0454270020a24f7588deb706c98e8d4
rhovian <atomicjab@proton.me>
2023-11-26 21:33:02 -0600

    a2

--- xnft/src/App.tsx
+++ xnft/src/App.tsx
@@ -2,14 +2,14 @@
 /* eslint-disable */
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
-// import { useUnityContext } from "react-unity-webgl";
+import { useUnityContext } from "react-unity-webgl";
 import { useConsoleInterceptor } from "./hooks/consoleOverride";
 // import { useLocation } from "react-router-dom";
 // import { extractMint } from "./utils";
 import FirstPage from "./screens/FirstPage";
-// import SecondPage from "./screens/SecondPage";
-// import ThirdPage from "./screens/ThirdPage";
-// import FourthPage from "./screens/FourthPage";
+import SecondPage from "./screens/SecondPage";
+import ThirdPage from "./screens/ThirdPage";
+import FourthPage from "./screens/FourthPage";
 // import { useSolanaConnection } from "./hooks";
 import { JsonMetadata, Metaplex, PublicKey } from "@metaplex-foundation/js";
 import "./App.css";
@@ -19,16 +19,16 @@ function App() {
   // const location = useLocation();
   const [mint] = useState<string | null>(null);
   const [page, setPage] = useState(0);
-  // const [metaplex, setMetaplex] = useState<Metaplex | null>(null);
+  const [metaplex, setMetaplex] = useState<Metaplex | null>(null);
   const [nftMetadata, setNftMetadata] = useState<JsonMetadata | null>(null);
   // const connection = useSolanaConnection();
 
-  // const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
-  //   loaderUrl: "build/SharkRun.loader.js",
-  //   dataUrl: "build/SharkRun.data.unityweb",
-  //   frameworkUrl: "build/SharkRun.framework.js.unityweb",
-  //   codeUrl: "build/SharkRun.wasm.unityweb",
-  // });
+  const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
+    loaderUrl: "build/SharkRun.loader.js",
+    dataUrl: "build/SharkRun.data.unityweb",
+    frameworkUrl: "build/SharkRun.framework.js.unityweb",
+    codeUrl: "build/SharkRun.wasm.unityweb",
+  });
 
   // useEffect(() => {
   //   if (location) setMint(extractMint(location));
@@ -64,45 +64,45 @@ function App() {
     setPage(2);
   };
 
-  // const goToFourthPage = () => {
-  //   if (!mint || !metaplex || !nftMetadata) return;
-  //   setPage(3);
-  // };
+  const goToFourthPage = () => {
+    if (!mint || !metaplex || !nftMetadata) return;
+    setPage(3);
+  };
 
   const renderPage = () => {
     switch (page) {
-      default:
+      case 0:
         return (
           <FirstPage
             gotoSecondPage={gotoSecondPage}
             handleQuickUpgrade={gotoSecondPage}
           />
         );
-      // case 1:
-      //   return <SecondPage handleStartGame={handleStartGame} />;
-      // case 2:
-      //   return (
-      //     <ThirdPage
-      //       unityProvider={unityProvider}
-      //       isLoaded={isLoaded}
-      //       loadingProgression={loadingProgression}
-      //     />
-      //   );
-      // case 3:
-      //   return (
-      //     <FourthPage
-      //       metaplex={metaplex!}
-      //       nftMetadata={nftMetadata!}
-      //       mint={mint!}
-      //     />
-      //   );
-      // default:
-      //   return (
-      //     <FirstPage
-      //       gotoSecondPage={gotoSecondPage}
-      //       handleQuickUpgrade={goToFourthPage}
-      //     />
-      //   );
+      case 1:
+        return <SecondPage handleStartGame={handleStartGame} />;
+      case 2:
+        return (
+          <ThirdPage
+            unityProvider={unityProvider}
+            isLoaded={isLoaded}
+            loadingProgression={loadingProgression}
+          />
+        );
+      case 3:
+        return (
+          <FourthPage
+            metaplex={metaplex!}
+            nftMetadata={nftMetadata!}
+            mint={mint!}
+          />
+        );
+      default:
+        return (
+          <FirstPage
+            gotoSecondPage={gotoSecondPage}
+            handleQuickUpgrade={goToFourthPage}
+          />
+        );
     }
   };
 
